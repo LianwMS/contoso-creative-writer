fead8f25f128ac21910dcbb8738880a89a9880b3
Liang Wang <lianw@microsoft.com>
2024-11-20 16:16:46 +0800

    Add Pipeline

--- infra/hooks/postprovision.sh
+++ infra/hooks/postprovision.sh
@@ -30,7 +30,7 @@ azd env set REACT_APP_API_BASE_URL $WEB_SERVICE_ACA_URI
 INTERNAL_ID=$(az cognitiveservices account show \
     --name ${AZURE_OPENAI_NAME} \
     -g ${AZURE_RESOURCE_GROUP} \
---query "properties.internalId" -o tsv)
+--query "properties.internalId" -o tsv) 
 
 # Construct the URL
 COGNITIVE_SERVICE_URL="https://oai.azure.com/portal/${INTERNAL_ID}?tenantid=${AZURE_TENANT_ID}"
@@ -39,14 +39,15 @@ echo "--- ✅ | 1. Post-provisioning - env configured ---"
 
 # Setup to run notebooks
 echo 'Installing dependencies from "requirements.txt"'
-python -m pip install -r src/api/requirements.txt > /dev/null
-python -m pip install ipython ipykernel > /dev/null      # Install ipython and ipykernel
-ipython kernel install --name=python3 --user > /dev/null # Configure the IPython kernel
-jupyter kernelspec list > /dev/null                      # Verify kernelspec list isn't empty
+python -m pip install -r src/api/requirements.txt
+python -m pip install ipython ipykernel                  # Install ipython and ipykernel
+ipython kernel install --name=python3 --user             # Configure the IPython kernel
+jupyter kernelspec list                                  # Verify kernelspec list isn't empty
+
 echo "--- ✅ | 2. Post-provisioning - ready execute notebooks ---"
 
 echo "Populating data ...."
-jupyter nbconvert --execute --to python --ExecutePreprocessor.timeout=-1 data/create-azure-search.ipynb > /dev/null
+jupyter nbconvert --execute --to python --ExecutePreprocessor.timeout=-1 data/create-azure-search.ipynb
 
 echo "--- ✅ | 3. Post-provisioning - populated data ---"
 
